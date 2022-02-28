b33925d3b2c1c282b12dfd4df7fb781c1091c381
Tewwi <ichduy188@gmail.com>
2022-02-28 11:07:17 +0700

    first commit

---
+++ src/modules/auth/components/LoginForm.tsx
@@ -0,0 +1,101 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Button, Checkbox, Container, FormControlLabel, TextField } from '@mui/material';
+import React from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { ILoginParams } from '../../../models/auth';
+
+const schema = yup
+  .object({
+    email: yup.string().required('Please Enter your Email'),
+    password: yup.string().required('Please Enter Your Password').min(5, 'Min 5 characters'),
+  })
+  .required();
+
+interface Props {
+  onLogin(values: ILoginParams): void;
+  loading: boolean;
+  errorMessage: string;
+}
+
+const LoginForm = (props: Props) => {
+  const { onLogin, loading, errorMessage } = props;
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<ILoginParams>({ resolver: yupResolver(schema) });
+
+  const onSubmit = React.useCallback((data: ILoginParams) => {
+    console.log(data);
+    return;
+  }, []);
+
+  return (
+    <Container component={'main'} maxWidth="xs">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Controller
+          render={({ field }) => (
+            <TextField
+              {...field}
+              margin="dense"
+              fullWidth
+              id="outlined-basic"
+              label="Email"
+              error={!!errors.email?.message}
+              helperText={errors.email?.message}
+              variant="outlined"
+              type="email"
+              className="materialUIInput"
+            />
+          )}
+          {...register('email', { required: true })}
+          name="email"
+          control={control}
+          defaultValue=""
+        />
+        <Controller
+          render={({ field }) => (
+            <TextField
+              {...field}
+              margin="dense"
+              fullWidth
+              id="outlined-basic"
+              label="Password"
+              error={!!errors.password?.message}
+              helperText={errors.password?.message || ''}
+              variant="outlined"
+              type="password"
+              className="materialUIInput"
+            />
+          )}
+          rules={{ required: true }}
+          name="password"
+          control={control}
+          defaultValue=""
+        />
+        <div>
+          <Controller
+            name="rememberMe"
+            control={control}
+            render={({ field }) => (
+              <FormControlLabel
+                {...field}
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+            )}
+          />
+        </div>
+        <div style={{ display: 'flex' }}>
+          <Button type="submit" style={{ margin: 'auto', width: '30%' }} variant="contained">
+            Login
+          </Button>
+        </div>
+      </form>
+    </Container>
+  );
+};
+
+export default LoginForm;
